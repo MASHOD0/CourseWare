2461c49e2e9515fae942aaeb20b97bc8b60d4378
MASHOD0 <mashhood2002@gmail.com>
2022-07-28 02:45:06 +0530

    add skeleton

--- src/main.py
+++ src/main.py
@@ -9,8 +9,42 @@ app.secret_key = 'aljflajfoAWHAOGAJ'
 
 # HOME PAGE
 @app.route('/')
-def hello():
-    return "hello world"
+def home():
+    """
+    returns the base home page, with login options
+    """
+    return render_template('home.html')
+
+@app.route('/student_signup')
+def student_signup():
+    """
+    returns the student signup page, with option to signup for the courseware
+    """
+    # TODO
+
+def faculty_signup():
+    """
+    returns the faculty signup page with option to signup for the college
+    """
+    # TODO
+
+def student_login():
+    """
+    returns the login page for students
+    """
+    # TODO
+
+def faculty_login():
+    """
+    returns login page for faculty
+    """
+    # TODO
+
+def admin_login():
+    """
+    returns the admin login page
+    """
+    # TODO
 
 if __name__ == '__main__':
     app.run()
