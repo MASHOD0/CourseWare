d2772e351730f2669888132c05baa2900d87d32d
MASHOD0 <mashhood2002@gmail.com>
2022-07-28 02:49:15 +0530

    add module 1 skeleton

--- src/main.py
+++ src/main.py
@@ -15,36 +15,42 @@ def home():
     """
     return render_template('home.html')
 
-@app.route('/student_signup')
+@app.route('/student_signup', methods=['GET', 'POST'])
 def student_signup():
     """
     returns the student signup page, with option to signup for the courseware
     """
     # TODO
 
+@app.route('/faculty_signup', methods=['GET', 'POST'])
 def faculty_signup():
     """
     returns the faculty signup page with option to signup for the college
     """
     # TODO
 
+@app.route('/student_login', methods=['GET', 'POST'])
 def student_login():
     """
     returns the login page for students
     """
     # TODO
 
+@app.route('/faculty_login', methods=['GET', 'POST'])
 def faculty_login():
     """
     returns login page for faculty
     """
     # TODO
 
+@app.route('admin_login', methods=['GET', 'POST'])
 def admin_login():
     """
     returns the admin login page
     """
     # TODO
+
+
 
 if __name__ == '__main__':
     app.run()
