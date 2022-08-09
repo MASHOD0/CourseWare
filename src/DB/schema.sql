3e97ef6cf15fbc5b4f10d8d9b88ee911ba2dcb8c
MASHOD0 <mashhood2002@gmail.com>
2022-08-09 13:20:17 +0530

    add student table

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -2,3 +2,13 @@ CREATE TABLE "department" (
   "department_id" SERIAL PRIMARY KEY,
   "department_name" varchar(20)
 );
+
+CREATE TABLE "student" (
+  "student_id" SERIAL PRIMARY KEY,
+  "section_id" int,
+  "usn" varchar(10),
+  "name" varchar(60),
+  "password" varchar(64),
+  "email" varchar(64),
+  "department_id" int
+);
